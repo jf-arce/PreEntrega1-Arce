508b1620a7b6e7888ebd9320803e02cf4d6f31c8
jf-arce <josefranciscoarce21@gmail.com>
2023-03-03 02:36:33 -0300

    Actualizando mainjs

--- js/main.js
+++ js/main.js
@@ -1,54 +1,165 @@
-let exit=1;
-let total=0;
-const nike=36000,adidas=30000,converse=26000,vans=28000,dc=25000;
+/*
+1) Menu:
+- Ver catalogo de productos
+- Comprar producto
+- Ver carrito // Terminar compra
+- Terminar la compra (Imprime un ticket de compra con los productos a llevar y el monto total).
 
-let msj=prompt("Quiere realizar una compra Si=1 No=0");
-if(msj==='1'){
-    while(exit!=0){
-        total+=venta();
-        exit=prompt("Quiere realizar otra compra? Si=1 No=0");
-    }
-    alert(`Precio final: ${total}`);
-    alert("Gracias por su compra!");
-}else{
-    alert("Gracias por visitarnos!");
+*/
+
+const zapatillas=[
+    {marca:"nike",modelo:"airforce", precio:50000,stock:30},
+    {marca:"nike",modelo:"jordan", precio:60000,stock:30},
+    {marca:"nike",modelo:"airmax", precio:38000,stock:30},
+    {marca:"nike",modelo:"vapormax", precio:40000,stock:30},
+    {marca:"nike",modelo:"uptempo", precio:45000,stock:30},
+   
+    {marca:"adidas",modelo:"galaxy6", precio:35000,stock:25},
+    {marca:"adidas",modelo:"superstar",precio:25000,stock:25},
+    {marca:"adidas",modelo:"forum",precio:30000,stock:25},
+    {marca:"adidas",modelo:"stansmith",precio:40000,stock:25},
+
+    {marca:"vans",modelo:"oldskool",precio:35000,stock:40},
+    {marca:"vans",modelo:"era",precio:30000,stock:40},
+    {marca:"vans",modelo:"sk8high",precio:38000,stock:40},
+
+    {marca:"converse",modelo:"allstar", precio:35000,stock:50},
+    {marca:"converse",modelo:"courtlandt",precio:30000,stock:50},
+    {marca:"converse",modelo:"chuck taylor all star",precio:38000,stock:50},
+
+    {marca:"dc",modelo:"midway", precio:40000,stock:20},
+    {marca:"dc",modelo:"manual sd", precio:38000,stock:20},
+    {marca:"dc",modelo:"kalisvulc", precio:35000,stock:20},
+];
+
+let carrito=[];
+
+//Se muestra el catalogo
+const catalogue=()=>{
+    let marca=prompt("Ingrese marca:")
+    let encontre=zapatillas.filter(product => product.marca===marca);
+    let message = 'Catálogo:\n';
+    
+    encontre.forEach(product => {
+        message+=`
+            marca: ${product.marca}
+            modelo: ${product.modelo}
+            precio: ${product.precio}
+        `
+        alert(message);
+     });
 }
 
-function venta(){
-    let precio;
-    marca=prompt("Ingrese la marca de zapatilla\nnike\nadidas (20% OFF)\nconverse\nvans\ndc (30% OFF)");
-    while(marca!=false){
-        switch(marca){
-            case 'nike':
-                talle=prompt("Elija el talle que desea: 39-44");
-                precio=nike;
-                marca=false;
-                break;
-            case 'adidas':
-                talle=prompt("Elija el talle que desea: 39-44");
-                precio=adidas-(adidas*0.2);
-                marca=false;
-                break;
-            case 'converse':
-                talle=prompt("Elija el talle que desea: 39-44");
-                precio=converse;
-                marca=false;
-                break;
-            case 'vans':
-                talle=prompt("Elija el talle que desea: 39-44");
-                precio=vans;
-                marca=false;
-                break;
-            case 'dc':
-                talle=prompt("Elija el talle que desea: 39-44");
-                precio=dc-(dc*0.3);
-                marca=false;
-                break;
-            default:
-                alert("Esa marca no esta disponible");
-                marca=prompt("Ingrese la marca de zapatilla\nnike\nadidas (20% OFF)\nconverse\nvans\ndc (30% OFF)");
+//ventas
+const buy_product=()=>{
+    let otroProd;
+    while(otroProd!==0){
+        let marca=prompt("Ingrese marca que desea comprar:");
+        let modelo=prompt("Elija algun modelo modelo que desea comprar:")
+        let encontre=zapatillas.find(product=> marca===product.marca && modelo===product.modelo);
+        let message=`
+        Producto:\n
+        marca: ${encontre.marca}
+        modelo: ${encontre.modelo}
+        precio: ${encontre.precio}
+        `
+        let cantidad=parseInt(prompt(`${message}\nCuantos pares va a llevar?`))
+        let message2=`Producto:\n
+        marca: ${encontre.marca}
+        modelo: ${encontre.modelo}
+        pares: ${cantidad}
+        precio: ${encontre.precio*cantidad}
+        `
+        let agregarCarrito=parseInt(prompt(`${message2}\nQuiere agregarlo al carrito Si=1 No=0`));
+        if(agregarCarrito===1){
+        carrito.push({
+            marca:encontre.marca,
+            modelo:encontre.modelo,
+            precio:encontre.precio,
+            cantidad:cantidad
+        });
+        otroProd=parseInt(prompt("Quiere agregar otro producto? Si=1 No=0"));
         }
     }
     
-    return precio;
 }
+
+//carrito
+const see_cart=()=>{
+    //Muestra el carrito de compras
+    let message="Carrito:\n";
+    carrito.forEach(products=>{
+        message+=`
+            marca: ${products.marca}
+            modelo: ${products.modelo}
+            pares: ${products.cantidad}
+            precio: ${products.precio*products.cantidad}
+        `
+        alert(message);
+    })
+    //
+    let finish=parseInt(prompt("1--> Comprar todo\n2--> Seguir comprando\n3--> Vaciar Carrito"));
+    if(finish===1){
+        let montoTotal=0;
+        carrito.forEach(prodCarrito=>{
+            montoTotal+=prodCarrito.precio*prodCarrito.cantidad;//Monto total de todos los productos
+        })
+        let confirmarCompra=parseInt(prompt(`Su monto total a pagar es de ${montoTotal} pesos\n1-->Pagar\n0-->Cancelar`));
+        if(confirmarCompra===1){
+            carrito.forEach(prodCarrito=>{
+                encontre=zapatillas.find(prodCatalogo=>prodCarrito.marca===prodCatalogo.marca && prodCarrito.modelo===prodCatalogo.modelo);
+                encontre.stock-=prodCarrito.cantidad; //Actualiza el stock del catalogo
+            })
+            alert("Gracias por su compra!")
+            carrito=[]; //Se reinicia el carrito
+        }else{
+            alert("Compra cancelada");
+        }
+    }else if(finish===3){
+        alert("Sus productos elejidos han sido removidos");
+        carrito=[];
+    }
+}
+
+
+//menu
+let option=prompt("Ingrese una opcion:\n1--> Ver catalogo\n2-->Comprar producto\n3-->Ver carrito\n4-->Salir");
+
+
+while (option !== '4') { // Ejecutar el bucle hasta que el usuario seleccione la opción de salir (4)
+
+    option = prompt("Ingrese una opcion:\n1--> Ver catalogo\n2-->Comprar producto\n3-->Ver carrito\n4-->Salir");
+
+    switch (option) {
+        case '1':
+            catalogue();
+            break;
+        case '2':
+            buy_product();
+            break;
+        case '3':
+            if(carrito.some(elemento => true)){
+                see_cart();
+            }else{
+                alert("El carrito esta vacio!");
+            }
+            break;
+        case '4':
+            break;
+        default:
+            alert("Opcion invalida");
+            break;
+    }
+}
+
+let message="Stock final del catalogo:\n"
+zapatillas.forEach(product => {
+    message+=`
+        marca: ${product.marca}
+        modelo: ${product.modelo}
+        precio: ${product.precio}
+        stock: ${product.stock}
+    `
+    alert(message);
+ });
+
